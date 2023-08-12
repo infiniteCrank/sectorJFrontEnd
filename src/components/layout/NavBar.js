fca0bd5594a2daf38f80c54db9975183447153bb
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-12 16:53:50 -0400

    display product types in top nav

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,6 +7,7 @@ import adminConfig from "../config/admin.json";
 function NavBar() {
   
   let [productTypes, setProductTypes] = useState(null)
+  let [productList, setProductList] = useState(null) 
 
   useEffect(() => {
 
@@ -22,38 +23,43 @@ function NavBar() {
     .then((response) =>{
       setProductTypes(response.data)
     })
+    .then(()=>{
+      if(productTypes){
+        const productTypeNameList = productTypes.map((productType) => {
+          return <li key={productType.name} >{productType.name}</li>
+        });
+        setProductList(productTypeNameList)
+      }
+    })
     .catch((err)=>{console.log(err)})
   },[productTypes])
 
   return (
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
     <Link className="navbar-brand" to="/">
           <img src={process.env.PUBLIC_URL + '/wizduds-logo-small.png'} alt="Wizduds - Extraordinary Clothing" height={100}/>
     </Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-        <Link class="nav-link active" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <Link className="nav-link active" to="/">Home</Link>
         </li>
-        <li class="nav-item dropdown">
-          <button class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <button className="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
             Products
           </button>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>Action</li>
-            <li>Another action</li>
-            <li><hr class="dropdown-divider"/></li>
-            <li>Something else here</li>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            {productList}
           </ul>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
