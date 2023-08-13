a7bacab335e9ce5a25cbee0ae48f1953a609038c
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-13 18:31:42 -0400

    update route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Products from './components/products/Products';
+import { useRef } from 'react';
 
 function App() {
+  const productsRef = useRef()
   return (
     <div className="App">
       <NavBar/>
-      <Route exact path='/' component={Products}/>
+      <Route path="/" render={(props) => <Products text="Hello, " {...props} />} />
     </div>
   );
 }
