e012a1c11608381be6f1d33ff5871c70fe98f3cf
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-13 18:29:02 -0400

    remove old code

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import {Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Products from './components/products/Products';
-// import Note from './components/notes/Note';
-// import NewNote from './components/notes/NewNote';
-// import LoginForm from './components/login/LoginForm';
-// import LogOut from './components/login/LogOut';
-// import SecuredRoute from './components/auth/SecuredRoute';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Route exact path='/' component={Products}/>
-      {/* <Route exact path='/blog/question/:noteId' component={Note}/>
-      <Route exact path='/blog/login' component={LoginForm}/>
-      <Route exact path='/blog/logout' component={LogOut}/>
-      <SecuredRoute path='/blog/new-note' component={NewNote} /> */}
     </div>
   );
 }
