b52f1c7355c5989e2709ac068cc26c31e227bc94
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-15 01:00:53 -0400

    optimize image loading

--- src/App.js
+++ src/App.js
@@ -14,30 +14,6 @@ function App() {
     setShoppingCart(data);
   }
 
-  let [productImages, setProductImages] = useState(null)
-  useEffect(() => {
-
-    axios.post('http://localhost:3000/login',adminConfig)
-    .then((tokenData)=>{
-    return {
-        headers: {'Authorization': tokenData.data.token},
-    }
-    })
-    .then((config)=>{
-    return axios.get('http://localhost:3000/product/images',config)
-    })
-    .then((response) =>{
-        const productImagesObject = response.data;
-        const newProductImages = {...productImages}
-        for (let i in productImagesObject) {
-            const productImage = productImagesObject[i];
-            newProductImages[productImage._id] = productImage.name +"."+ productImage.imageType;
-        }
-        setProductImages(newProductImages)
-    })
-    .catch((err)=>{console.log(err)})
-  },[productImages])
-
   let [products, setProducts] = useState(null)
   let [productsMap, setProductsMap] = useState({})
 
@@ -69,12 +45,10 @@ function App() {
       <NavBar 
         shoppingCart={shoppingCart} 
         saveCart={saveCart} 
-        productImages={productImages}
         productsMap={productsMap}
       />
       <Route path="/" render={(props) => 
       <Products 
-          productImages={productImages} 
           shoppingCart={shoppingCart} 
           products={products}
           productsMap={productsMap}
