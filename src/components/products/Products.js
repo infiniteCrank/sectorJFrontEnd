887aa245869f0b41f3dbe56773352991d538f80a
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-16 19:07:43 -0400

    makes view product modal work

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -59,6 +59,7 @@ function Products({shoppingCart, saveCart, productsMap, products, quantityMap, s
     }
 
     return (
+    <>
     <div className="container">
         <div className="row">
         {
@@ -89,6 +90,8 @@ function Products({shoppingCart, saveCart, productsMap, products, quantityMap, s
                     <div className='container-fluid'>
                         <button 
                             className="btn btn-outline-dark"
+                            data-bs-toggle="modal" 
+                            data-bs-target="#viewProductModal"
                         >
                             View
                         </button>
@@ -107,6 +110,25 @@ function Products({shoppingCart, saveCart, productsMap, products, quantityMap, s
         }
         </div>
     </div>
+
+    <div className="modal fade" id="viewProductModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                ...
+                </div>
+                <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
     );
 }
 
